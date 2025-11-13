1a375f528fc88306c48608dc851d7f125a1eff9f
itzel <salas2210187@uabc.edu.mx>
2025-11-12 23:46:26 -0800

    First commit

---
+++ EXTRACCION/api_desempleo.py
@@ -0,0 +1,26 @@
+import requests
+import pandas as pd
+
+def extraer_api(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()["data"]
+
+        d = {"State": [], "Quarter": [], "Sex": [], "Workforce":[]}
+
+        for item in data:
+            d["State"].append(item["State"])
+            d["Quarter"].append(item["Quarter"])
+            d["Sex"].append(item["Sex"])
+            d["Workforce"].append(item["Workforce"])
+        df = pd.DataFrame(d)
+        return df
+    else:
+        print("ERROR")
+
+
+if __name__ == "__main__":
+    df1 = extraer_api("https://www.economia.gob.mx/datamexico/api/data?State=2&Economically+Active+Population=1&Population+Classification=2&cube=inegi_enoe&drilldowns=State,Quarter,Sex&measures=Workforce&locale=es&parents=false")
+    df2 = extraer_api("https://www.economia.gob.mx/datamexico/api/data?State=2&Economically+Active+Population=1&Population+Classification=2&cube=inegi_enoe&drilldowns=State,Quarter,Sex&measures=Workforce&locale=es&parents=false")
+    df_desempleo = pd.concat([df1, df2])
+    df_desempleo.to_csv("datasets/api_desempleo.csv")
