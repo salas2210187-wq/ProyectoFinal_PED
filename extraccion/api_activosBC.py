50ec5db2ace1fe5a21a4fb298a802289093c8b1d
itzel <salas2210187@uabc.edu.mx>
2025-11-13 19:46:43 -0800

    Actualización API

--- extraccion/api_activosBC.py
+++ extraccion/api_activosBC.py
@@ -6,13 +6,13 @@ def extraer_api(url):
     if response.status_code == 200:
         data = response.json()["data"]
 
-        d = {"State": [], "Quarter": [], "Sex": [], "Workforce":[]}
+        d = {"State": [], "Quarter": [], "Sex": [], "Total":[]}
 
         for item in data:
             d["State"].append(item["State"])
             d["Quarter"].append(item["Quarter"])
             d["Sex"].append(item["Sex"])
-            d["Workforce"].append(item["Workforce"])
+            d["Total"].append(item["Workforce"])
         df = pd.DataFrame(d)
         return df
     else:
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     df1 = extraer_api("https://www.economia.gob.mx/datamexico/api/data?State=2&Economically+Active+Population=1&Population+Classification=2&cube=inegi_enoe&drilldowns=State,Quarter,Sex&measures=Workforce&locale=es&parents=false")
     df2 = extraer_api("https://www.economia.gob.mx/datamexico/api/data?State=2&Economically+Active+Population=1&Population+Classification=2&cube=inegi_enoe&drilldowns=State,Quarter,Sex&measures=Workforce&locale=es&parents=false")
     df_desempleo = pd.concat([df1, df2])
-    df_desempleo.to_csv("datasets/api_desempleo.csv")
+    df_desempleo.to_csv("datasets/total_activosBC.csv")
